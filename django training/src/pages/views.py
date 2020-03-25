476b3f7ed80eba5b9b0095de4047f05d11e433e4
Draxgen <48406465+Draxgen@users.noreply.github.com>
2020-03-26 00:00:20 +0100

    django update

--- django training/src/pages/views.py
+++ django training/src/pages/views.py
@@ -10,4 +10,10 @@ def contact_view(request, *args, **kwargs):
 	return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-	return render(request, "about.html", {})
+	my_context = {
+		'my_text': 'this is about us <3',
+		'my_number': 123,
+		'this_is_true': True,
+		'my_list':[1,123,12345,1234567]
+	}
+	return render(request, "about.html", my_context)
