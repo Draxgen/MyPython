8c3278e15e362d043617535ff700843799a6b2a2
Draxgen <48406465+Draxgen@users.noreply.github.com>
2020-03-28 22:16:17 +0100

    pandas sales training complete

--- pandas training/sales training/sales.py
+++ pandas training/sales training/sales.py
@@ -50,3 +50,68 @@ plt.xticks(cities['City'], rotation='vertical')
 plt.ylabel('Sales [$]')
 plt.xlabel('City name')
 plt.show()
+
+#%% Question #3: What time should we display advertisements to maximize the likelihood of purchases?
+df['Order Date'] = pd.to_datetime(df['Order Date'])
+df['Hour'] = df['Order Date'].dt.hour
+df['Minute'] = df['Order Date'].dt.minute
+
+hours = [hour for hour, dtfrm in df.groupby('Hour')]
+
+plt.plot(hours, df.groupby('Hour').count())
+plt.xticks(hours)
+plt.xlabel('Hour')
+plt.ylabel('Number of Orders')
+plt.grid()
+plt.show()
+
+#%% Question #4: Which products are most often sold together?
+df2 = df[df['Order ID'].duplicated(keep=False)]
+
+temp_df = df2.groupby('Order ID')
+
+new_column = temp_df['Product'].transform(lambda x: ','.join(x))
+                                        
+df2.insert(11, 'Grouped', new_column)
+
+df2 = df2[['Order ID','Grouped']].drop_duplicates()
+
+from itertools import combinations
+from collections import Counter
+
+count = Counter()
+
+for row in df2['Grouped']:
+    row_list = row.split(',')
+    count.update(Counter(combinations(row_list, 2)))
+    
+for key,value in (count.most_common(10)):
+    print(key, value)
+
+#%% Question 5: What product sold the most and why?
+df['Quantity Ordered'] = df['Quantity Ordered'].astype('int32') 
+df['Price Each'] = df['Price Each'].astype('float') 
+product_group = df[['Product','Quantity Ordered']].groupby('Product').sum()
+
+plt.bar(product_group.index, product_group['Quantity Ordered'])
+plt.xticks(product_group.index, rotation='vertical')
+plt.ylabel('Quantity Ordered')
+plt.xlabel('Product')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
