7558996394279a1a3a474274c58e9161070b3d09
Draxgen <48406465+Draxgen@users.noreply.github.com>
2020-04-07 16:08:36 +0200

    update download manager

--- scripts/downloadmanager.py
+++ scripts/downloadmanager.py
@@ -1,19 +1,25 @@
+import time
 import os
-import watchdog
-import watchdog.events
-import watchdog.observers
+import logging
+import sys
+from watchdog.events import LoggingEventHandler
+from watchdog.events import FileCreatedEvent
+from watchdog.observers import Observer
 
-# set the user's download dir as current working dir
-home_dir = os.path.expanduser("~")
-download_dir = os.path.join(home_dir, 'Downloads')
-os.chdir(download_dir)
-print(os.getcwd())
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, 
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
 
-#setup and start the observer
-event_hndlr = watchdog.events.LoggingEventHandler()
-observer = watchdog.observers.Observer()
-observer.schedule(event_hndlr, download_dir, recursive=False)
-observer.start()
+    path = os.path.expanduser(r'~\Downloads')
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path)
+    observer.start()
 
-#listen for new file
-
+    try:
+        while True:
+            time.sleep(1)
+    except(KeyboardInterrupt):
+        observer.stop()
+    observer.join()
